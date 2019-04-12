ab552ac62e77bf7e60afd8d4a6cbfbd4451cc5ec
yannsheriff <yanncherif@live.fr>
2019-04-12 10:23:28 +0200

    implement skill

--- src/components/complexe/Skill/Skill.jsx
+++ src/components/complexe/Skill/Skill.jsx
@@ -1,15 +1,46 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
+import { winGame, endGame } from '../../../redux/actions/steps';
 import './Skill.scss';
 
 
 class Skill extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      endNaration: false,
+      didWin: false,
+    };
+  }
+
+  chooseSkill = (skill) => {
+    const { data, winGame } = this.props;
+    if (skill !== data.adventure_skill) {
+      this.setState({ endNaration: data.adventure_victory, didWin: true });
+    } else {
+      this.setState({ endNaration: data.adventure_defeat });
+    }
+  }
+
   render() {
+    const { profil, endGame } = this.props;
+    const { endNaration, didWin } = this.state;
+    const skills = profil.skills.map(element => (<button onClick={() => this.chooseSkill(element.title)}>{element.title}</button>));
+
     return (
       <div id="skill">
-        <p>Skill</p>
-        <button onClick={this.props.next}>next</button>
-        <button onClick={this.props.fail}>fail</button>
+        <p>Quel skill voulez vous choisir pour Faire cette action ? </p>
+        {!endNaration && skills}
+        {endNaration
+         && (
+         <div>
+           <div dangerouslySetInnerHTML={{ __html: documentToHtmlString(endNaration) }} />
+           <button onClick={() => { endGame(didWin ? 'win' : 'loose'); }}>next </button>
+         </div>
+         )
+        }
       </div>
     );
   }
@@ -20,12 +51,22 @@ class Skill extends Component {
   ================================================================ */
 
 const mapStateToProps = state => ({
-  mainState: state.mainReducer,
+  profil: state.profilReducer,
+});
+
+const mapDispatchToProps = dispatch => ({
+  winGame: () => {
+    dispatch(winGame());
+  },
+  endGame: (reason) => {
+    dispatch(endGame(reason));
+  },
 });
 
 
 const componentContainer = connect(
   mapStateToProps,
+  mapDispatchToProps,
 )(Skill);
 
 export default componentContainer;
