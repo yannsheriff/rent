a5bfcd8a0c89e588cade187557f7a5dd5f1e76fe
audrenmp <audren.mp@gmail.com>
2019-05-13 15:17:36 +0200

    visit

--- src/components/card-template/Visit/Visit.jsx
+++ src/components/card-template/Visit/Visit.jsx
@@ -11,7 +11,7 @@ class Visit extends Component {
       <div className="card--content card--visit">
         <h2 className="card--type">Visite</h2>
         {/* <img className="card--illu" src={ads} alt="" /> */}
-        <h1>{ data.visit.visit_quality }</h1>
+        <h1>{ data.visit.visit_title }</h1>
         <div dangerouslySetInnerHTML={{ __html: documentToHtmlString(data.visit.visit_description) }} />
       </div>
     );
