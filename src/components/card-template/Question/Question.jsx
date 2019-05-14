65ed17bd815e9259b676faaaac7cccf5186f66c0
yannsheriff <yanncherif@live.fr>
2019-05-14 15:10:44 +0200

    add lottie to question component

--- src/components/card-template/Question/Question.jsx
+++ src/components/card-template/Question/Question.jsx
@@ -3,14 +3,32 @@
 import React, { Component } from 'react';
 import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
 import './Question.scss';
+import lottie from 'lottie-web';
+import animations from 'assets/animation';
+
 
 class Question extends Component {
+  constructor(props) {
+    super(props);
+    this.animationContainer = React.createRef();
+  }
+
+  componentDidMount() {
+    lottie.loadAnimation({
+      container: this.animationContainer.current, // the dom element that will contain the animation
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: animations.question, // the path to the animation json
+    });
+  }
+
   render() {
     const { data } = this.props;
     return (
       <div className="card--content card--question">
-        <h2 className="card--type">Remise en question</h2>
         {/* <img className="card--illu" src={question} alt="" /> */}
+        <div className="animation card--illu" ref={this.animationContainer} />
         <div
           dangerouslySetInnerHTML={
                 { __html: documentToHtmlString(data.question_narration) }
