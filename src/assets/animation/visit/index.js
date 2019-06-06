a3f8489d83dee6dc35c9942b7c08821e8d8c7160
audrenmp <audren.mp@gmail.com>
2019-06-06 18:38:40 +0200

    assets

---
+++ src/assets/animation/visit/index.js
@@ -0,0 +1,9 @@
+import visitOk from './visit_ok.json';
+import visitGood from './visit_good.json';
+import visitBad from './visit_good.json';
+
+export default {
+  visitOk,
+  visitGood,
+  visitBad,
+};
