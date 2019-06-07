39688fcd486d0914587d90db1db347c224cb0d45
yannsheriff <yanncherif@live.fr>
2019-06-07 17:38:09 +0200

    Activate api

--- src/services/SocrateService.js
+++ src/services/SocrateService.js
@@ -6,7 +6,7 @@ let instance = null;
 
 class SocrateServices {
   constructor() {
-    // this.apiUrl = 'http://vps.yannischerif.com:4000/api';
+    this.apiUrl = 'http://vps.yannischerif.com:4000/api';
     // this.apiUrl = 'http://localhost:4000/api';
 
     if (!instance) {
