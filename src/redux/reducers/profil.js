fc08e54ae8b1a8f2c8906c281ee8f1fb0eac3526
audrenmp <audren.mp@gmail.com>
2019-06-10 23:09:03 +0200

    reset

--- src/redux/reducers/profil.js
+++ src/redux/reducers/profil.js
@@ -36,7 +36,7 @@ const defaultState = {
   premium: false,
 };
 
-export function profilReducer(state = debugProfil, action) {
+export function profilReducer(state = defaultState, action) {
   switch (action.type) {
     case UPDATE_STATUS: {
       const newStatus = action.payload;
