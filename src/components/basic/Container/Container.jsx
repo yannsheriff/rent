260065741dc99fc0f85ad44ea51eb2ad4e76d100
audrenmp <audren.mp@gmail.com>
2019-11-06 11:20:10 +0100

    WIP : responsive desktop setup

---
+++ src/components/basic/Container/Container.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
+import './Container.scss';
+
+function Container(props) {
+  return (
+    <div id="container">
+      super
+    </div>
+  );
+}
+
+
+export default Container;
